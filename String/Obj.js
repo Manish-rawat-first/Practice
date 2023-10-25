758686a7a9e811701bec1bead9f578af5c538626
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-25 03:43:29 +0000

    Ob

--- String/Obj.js
+++ String/Obj.js
@@ -1,3 +1,16 @@
 const ob = {}
 ob.greeting = ()=>"Hello"
 console.log(ob.greeting)
+
+let ob1 = {1:"Hello",2:"Ji"}
+let ob2 = {3:"Zero",4:"Motorola"}
+let ob3 = {...ob1,...ob2};
+console.log(ob3)
+
+let o4 = {
+    grreting:{
+        first:"Manish",
+        last:"rawat"
+    }
+}
+console.log(`${o4.grreting.first}`)
